fd522fa688028534c813ff8843bff5762fa8aa6b
rahuljain46 <rahuljain820436@gmail.com>
2020-05-02 01:53:15 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider as ReduxProvider } from "react-redux";
 
 render(
