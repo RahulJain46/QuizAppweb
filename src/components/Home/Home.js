0fb5b1fad8aefe7ec5448274b89931087c89ff06
rahuljain46 <rahuljain820436@gmail.com>
2020-05-13 18:34:41 +0530

    quiz current date

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -65,7 +65,7 @@ function Home() {
       ? new Date().getDate()
       : "0" + new Date().getDate();
   const year = new Date().getFullYear();
-  const currentMonth = new Date().getMonth();
+  const currentMonth = new Date().getMonth() + 1;
   const month = date
     .toLocaleString("default", { month: "short" })
     .toUpperCase();
