604bbf48ddf212420cc311b133260d7e7173ce5c
anuj96 <anujjaincoolbuddy@gmail.com>
2020-05-26 02:01:07 +0530

    bhajan page creation

--- src/components/Bhajan.js
+++ src/components/Bhajan.js
@@ -1,204 +1,112 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import { Link } from "react-router-dom";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
-import Button from "@material-ui/core/Button";
-import Link from "@material-ui/core/Link";
-import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
-import Typography from "@material-ui/core/Typography";
-import Fade from "@material-ui/core/Fade";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import TablePagination from "@material-ui/core/TablePagination";
 import TableRow from "@material-ui/core/TableRow";
-import { links } from "../Config";
 
 const useStyles = makeStyles(theme => ({
-  tableheading: {
-    // width: "%",
-    position: "fixed",
-    top: 151,
-    left: "4%",
-    bottom: "9%",
-    minHeight: 1000
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center"
   },
-  backButton: {
-    backgroundColor: "#1976d2"
+  button: {
+    backgroundColor: "#1976d2",
+    color: "#fff",
+    width: 188,
+    "&:hover": {
+      backgroundColor: "#303f9f"
+    }
   },
-  headerDate: {
-    fontWeight: 600,
+  topInstruction:{
+    color: "#ac0b0b",
+    marginBottom: 10,
+    fontSize: 20
+    
+  },
+  formContainer: {
     textAlign: "center",
-    fontSize: 23
+    marginTop: 18
   },
-  headerBackButton: {
-    textAlign: "left",
-    fontWeight: 600,
-    textAlign: "center",
-    fontSize: 23
+  message: {
+    marginBottom: 10,
+    fontSize: 19,
+    fontWeight: 500,
+    textAlign: "left"
   },
-  container: {
-    maxHeight: "82%",
-    // position: "fixed",
-    left: "19%"
+  instruction: {
+    flexGrow: 1,
+    marginTop: 152,
+    position: "absolute",
+    marginBottom: 73,
+    left: "25%",
+    right: "25%"
   },
-  tablecolumns: {
-    background: "blue"
-  },
-  tableNumber: {
-    minWidth: 200,
-    backgroundColor: "#e9ecef"
-  },
-  tableQuestion: {
-    minWidth: 80,
-    backgroundColor: "#e9ecef"
-  },
-  tableAnswer: {
-    minWidth: 80,
-    backgroundColor: "#e9ecef"
-  },
-  tableRemarks: {
-    minWidth: 100,
-    backgroundColor: "#e9ecef"
-  },
-  [theme.breakpoints.down("1124")]: {
-    tableheading: {
-      top: 6,
-      left: 0,
-      right: 0,
-      paddingRight: 0,
-      position: "relative"
+  [theme.breakpoints.down("1123")]: {
+    instruction: {
+      width: "100%",
+      left: "0%",
+      right: "0%",
+      top: "0%"
     },
-    container: {
-      left: 0,
-      maxHeight: "70%",
-      minHeight: 1000
+    quizitems: {
+      maxWidth: "100%",
+      padding: "0px ! important",
+      paddingTop: "10px ! important"
     },
-    tableNumber: {
-      fontSize: 15,
-      borderRight: "1px solid",
-      borderLeft: "1px solid",
-      borderTop: "1px solid",
-      paddingRight: 0,
-      paddingLeft: 5,
-      minWidth: 100
+    button: {
+      padding: "11px 6px",
+      width: 185
     },
-    tableQuestion: {
-      fontSize: 15,
-      borderRight: "1px solid",
-      borderTop: "1px solid",
-      paddingRight: 0,
-      minWidth: 200,
-      paddingLeft: 9
+    form: {
+      display: "inline-block"
     },
-    tableAnswer: {
-      fontSize: 15,
-      minWidth: 80,
-      borderRight: "1px solid",
-      borderTop: "1px solid",
-      paddingRight: 0,
-      paddingLeft: 4
+    formContainer: {
+      textAlign: "center",
+      marginTop: 18
     },
-    tableRemarks: {
-      fontSize: 15,
-      minWidth: 80,
-      borderRight: "4px solid",
-      borderTop: "1px solid",
-      paddingRight: 0,
-      paddingLeft: 4
-    },
-    tableQuestionCell: {
-      fontSize: 15,
-      borderRight: "1px solid",
-      paddingLeft: 5,
-      paddingRight: 5,
-      paddingTop: 10,
-      paddingBottom: 10,
-      lineHeight: "21px"
-    },
-    tableAnswerCell: {
-      fontSize: 15,
-      borderRight: "1px solid",
-      paddingLeft: 5,
-      paddingRight: 5,
-      paddingTop: 10,
-      paddingBottom: 10,
-      lineHeight: "21px",
-      textAlign: "-webkit-center"
-    },
-    tableRemarkCell: {
-      fontSize: 15,
-      borderRight: "1px solid",
-      paddingLeft: 5,
-      paddingRight: 5,
-      paddingTop: 10,
-      paddingBottom: 10,
-      lineHeight: "21px",
-      borderRight: "4px solid"
-    },
-    tableNumberCell: {
-      fontSize: 15,
-      borderRight: "1px solid",
-      paddingLeft: 5,
-      paddingRight: 5,
-      paddingTop: 10,
-      paddingBottom: 10,
-      lineHeight: "27px",
-      borderLeft: "1px solid"
-    },
-    tableHeader: {
-      marginLeft: 3
-    },
-    backButton: {
-      backgroundColor: "#1976d2",
-      padding: "3px 10px",
-      fontSize: 11
-    },
-    backArrow: {
-      fontSize: 15
+    message: {
+      marginBottom: 14,
+      fontSize: 18,
+      fontWeight: 500
     }
   },
   [theme.breakpoints.down("361")]: {
-    container: {
-      maxHeight: "70%"
+    formContainer: {
+      textAlign: "center",
+      marginTop: 18
+    },
+    message: {
+      marginBottom: 14,
+      fontSize: 16,
+      fontWeight: 500
+    },
+    topInstruction: {
+      fontSize: 17
     }
   }
 }));
 
-export default function QuizAnswer1(props) {
+function Bhajan() {
   const classes = useStyles();
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-  const date = props.match.params.date;
-  const [loading, setLoading] = useState(true);
-
-  const [answers, setAnswers] = useState([]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  useEffect(() => {
-    const questionsArray = [];
-    fetch(links.backendURL + "bhajan")
-      .then(answerJson => {
-        return answerJson.json();
-      })
-      .then(answers => {
-        answers.map(answer => {
-          questionsArray.push(answer);
-        });
-        setAnswers(questionsArray);
-        setLoading(false);
-      });
-  }, []);
-
   return (
-    <Paper className={classes.tableheading}>
-      {answers.length != 0 && !loading ? (
-        <TableContainer className={classes.container}>
+<div className={classes.instruction}>
+<Paper className={classes.tableheading}>
+    <TableContainer className={classes.container}>
           <Table
             stickyHeader
             aria-label="sticky table"
@@ -207,64 +115,49 @@ export default function QuizAnswer1(props) {
             <TableHead className={classes.tablecolumns}>
               <TableRow>
                 <TableCell key="name" className={classes.tableNumber}>
-                  Link
+                भजन नाम
                 </TableCell>
                 <TableCell key="name" className={classes.tableQuestion}>
-                  भजन नाम
+                गायक
                 </TableCell>
                 <TableCell key="code" className={classes.tableAnswer}>
-                  गायक
+                गीतकार
                 </TableCell>
                 <TableCell key="population" className={classes.tableRemarks}>
-                  गीतकार
+                  Link
                 </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {answers.map(answer => (
-                <TableRow>
-                  <TableCell className={classes.tableNumberCell}>
-                    <Link
-                      href={answer.link}
-                      target="_blank"
-                      rel="noopener noreferrer"
+              {
+                
+                  <TableRow>
+                    <TableCell className={classes.tableNumberCell}>
+                    मेरा जीवन आज भगवन धन्य हो गया..
+                    </TableCell>
+                    <TableCell
+                      component="th"
+                      scope="row"
+                      className={classes.tableQuestionCell}
                     >
-                      <label className={classes.helpLabel}>
-                        Click here to listen
-                      </label>
-                    </Link>
-                  </TableCell>
-                  <TableCell
-                    component="th"
-                    scope="row"
-                    className={classes.tableQuestionCell}
-                  >
-                    {answer.bhajanName}
-                  </TableCell>
-                  <TableCell className={classes.tableAnswerCell}>
-                    {answer.singer}
-                  </TableCell>
-                  <TableCell className={classes.tableRemarkCell}>
-                    {answer.writer}
-                  </TableCell>
-                </TableRow>
-              ))}
+                      अनिमेष जैन 
+            
+                    </TableCell>
+                    <TableCell className={classes.tableAnswerCell}>
+                    सुरेश चंद्र जैन
+                    </TableCell>
+                    <TableCell className={classes.tableRemarkCell}>
+                      link
+                    </TableCell>
+                  </TableRow>
+                
+              }
             </TableBody>
           </Table>
         </TableContainer>
-      ) : (
-        <div className={classes.loading}>
-          <Fade
-            in={loading}
-            style={{
-              transitionDelay: loading ? "800ms" : "0ms"
-            }}
-            unmountOnExit
-          >
-            <CircularProgress />
-          </Fade>
-        </div>
-      )}
-    </Paper>
+        </Paper>
+    </div>
   );
 }
+
+export default Bhajan;
