80abce0fe44d7e536049260946ced83641f6171a
Rahul Jain <rahuljain820436@gmail.com>
2020-05-25 10:39:58 +0530

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,3 @@
 export const links = {
-  // backendURL: "https://prodbackendjin.herokuapp.com/"
-  // backendURL: "http:localhost:3001"
-  // backendURL: "https://backenddevelop.herokuapp.com/"
-  backendURL: "https://testbackenddarshan.herokuapp.com/"
+  backendURL: "https://sanganerbackend.herokuapp.com/"
 };
