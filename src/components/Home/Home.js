5d4cbec4416984196f7aa428779410ca395eaa13
Rahul Jain <rahuljain820436@gmail.com>
2020-07-05 17:06:22 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -449,15 +449,7 @@ function Home() {
             </Paper>
           </Link>
         </Grid>
-        <Grid item xs={12} className={classes.quizitems}>
-          <Link to="/childrenquiz">
-            <Paper className={classes.paper}>
-              <Button variant="contained" className={classes.topicButton}>
-                Children Quiz
-              </Button>
-            </Paper>
-          </Link>
-        </Grid>
+        
         <Grid item xs={6} className={classes.quizitems}>
           <Link to="/oldquizresults">
             <Paper className={classes.paper}>
