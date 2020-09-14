7ceb1a72e471cd8b77032e7f9ac202c500eda312
rahuljain46 <rahuljain820436@gmail.com>
2020-09-14 11:27:39 +0530

    class feedback

--- src/components/QuizForm.js
+++ src/components/QuizForm.js
@@ -363,6 +363,9 @@ function QuizForm(props) {
     userResponseJson["userId"] = uuid;
     userResponseJson["feedback"] = myMap.get("feedback");
     userResponseJson["suggestion"] = myMap.get("suggestion").trim();
+    userResponseJson["classsuggestion"] = myMap.get("classsuggestion").trim();
+    userResponseJson["classcount"] = myMap.get("classcount").trim();
+    userResponseJson["classattendance"] = myMap.get("classattendance").trim();
     userResponseJson = calcaulateScore(ques, myMap, userResponseJson);
     const date = props.match.params.date;
     let usersJson = {};
@@ -721,6 +724,65 @@ function QuizForm(props) {
                   )}
                 </CardContent>
               </Card>
+              <Card className={classes.questionfields}>
+                <CardContent>
+                  <fieldset className={classes.questionContent}>
+                    <label className={classes.label}>
+                      {
+                        "प्रतिदिन शाम को 8:15 पर इष्टोपदेश ग्रन्थ का सरल व् सुबोध तरीके से पंडित सुरेश जी जैन द्वारा ज़ूम app पर जल्द ही निःशुल्क पढ़ाने हेतु क्लास चालू करने  की योजना है . क्या आप इस क्लास में भाग लेने के इच्छुक है ?"
+                      }
+                    </label>
+                  </fieldset>
+
+                  <fieldset className={classes.questionOption}>
+                    <label className={classes.optionLabel}>
+                      <input
+                        type="radio"
+                        className={classes.radioButton}
+                        value="YES"
+                        name="classattendance"
+                        ref={register({ required: false })}
+                        label="YES"
+                      />
+                      YES
+                    </label>
+                  </fieldset>
+
+                  <fieldset className={classes.questionOption}>
+                    <label className={classes.optionLabel}>
+                      <input
+                        type="radio"
+                        className={classes.radioButton}
+                        value="NO"
+                        name="classattendance"
+                        ref={register({ required: false })}
+                        label="NO"
+                      />
+                      NO
+                    </label>
+                  </fieldset>
+                  <label className={classes.label}>
+                    {" "}
+                    संभावित सदस्य संख्या :{" "}
+                  </label>
+                  <input
+                    className={classes.input}
+                    placeholder="Your answer"
+                    name="classcount"
+                    ref={register({ required: false })}
+                  />
+                  <label className={classes.label}>
+                    उक्त क्लास के बारे में आपका कोई सुझाव हो तो लिखे :
+                  </label>
+                  <input
+                    className={classes.input}
+                    placeholder="Your answer"
+                    name="classsuggestion"
+                    ref={register({ required: false })}
+                  />
+                </CardContent>
+              </Card>
+
               <label className={classes.label}>टिप / सुझाव</label>
               <input
                 className={classes.input}
