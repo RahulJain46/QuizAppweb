bccf45eb54897a605eb02549501185c191584491
Rahul Jain <rahuljain820436@gmail.com>
2023-06-26 09:20:52 +0530

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,5 @@
 export const links = {
-  backendURL: "https://jindarshan.in/"
+  backendURL: "http://jindarshan.in/"
   //backendURL: "https://developbackend.herokuapp.com/"
   // backendURL: "https://localhost:3001/"
   //backendURL: "https://prodbackendjin.herokuapp.com/"
